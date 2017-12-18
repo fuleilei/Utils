f175fa9388608a448328d630ef4f802e1a8d40e0
fuleilei <fuleilei@mingtian.com>
2017-12-18 18:01:38 +0800

    我的github项目

---
+++ androidUtils/mylibrary/src/main/java/com/github/mine/mylibrary/AndroidUtils.java
@@ -0,0 +1,24 @@
+package com.github.mine.mylibrary;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by fuleilei on 2017/12/18.
+ */
+
+public class AndroidUtils {
+    private static DisplayMetrics sDisplay;
+    private static final String KEY_MIUI_VERSION_NAME = "ro.miui.ui.version.name";
+
+    /**
+     * @return 屏幕高度
+     */
+    public static int getScreenHeight(Context context) {
+        if (sDisplay == null) {
+            sDisplay = context.getResources().getDisplayMetrics();
+        }
+        int height = sDisplay.heightPixels;
+        return height;
+    }
+}
